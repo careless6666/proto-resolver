9886643c291fc07e5c39faf1e72e45e512c94852
kupes <kupes@kupes.com>
2024-06-17 23:48:05 +0300

    wip

--- internal/parser_tests.go
+++ internal/parser_tests.go
@@ -1,1 +1,18 @@
 package main
+
+//mock data
+
+/* happy path
+version: v1
+deps:
+  - github.com/googleapis/googleapis/google/api/http.proto v0.0.0-20211005231101-409e134ffaac
+  - github.com/googleapis/googleapis/google/api/annotations.proto v0.0.0-20211005231101-409e134ffaac
+
+variations of format
+
+deps:
+  - github.com/googleapis/googleapis/google/api v0.0.0-20211005231101-409e134ffaac
+  - github.com/googleapis/googleapis/google/api/* v0.0.0-20211005231101-409e134ffaac
+  - github.com/googleapis/googleapis/google/api/* v1.0.0
+
+*/
