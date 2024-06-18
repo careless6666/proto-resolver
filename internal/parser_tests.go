7f58657fdd4ec522df96e5f8946c29cf4a35e779
kupes <kupes@kupes.com>
2024-06-18 15:43:59 +0300

    fix formats

--- internal/parser_tests.go
+++ internal/parser_tests.go
@@ -5,8 +5,11 @@ package main
 /* happy path
 version: v1
 deps:
-  - github.com/googleapis/googleapis/google/api/http.proto v0.0.0-20211005231101-409e134ffaac
-  - github.com/googleapis/googleapis/google/api/annotations.proto v0.0.0-20211005231101-409e134ffaac
+  - git: github.com/googleapis/googleapis/google/api/http.proto v0.0.0-20211005231101-409e134ffaac
+  - git: github.com/googleapis/googleapis/google/api/annotations.proto v0.0.0-20211005231101-409e134ffaac
+  - remote_file: https://github.com/googleapis/googleapis/blob/master/google/api/annotations.proto
+  - local_file: /tmp/path
+ */
 
 variations of format
 
