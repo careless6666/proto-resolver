5d5e0faf7de86f58cf9630905c1e3d440e20e07e
kupes <kupes@kupes.com>
2024-07-03 00:25:21 +0300

    wip

--- internal/models/models.go
+++ internal/models/models.go
@@ -21,3 +21,18 @@ type VersionInfo struct {
 	Tag            string
 	CommitRevision string
 }
+
+type DependencyList struct {
+	Type           string `json:"type"`
+	Source         string `json:"source"`
+	RelativePath   string `json:"RelativePath"`
+	Version        string `json:"version"`
+	Tag            string `json:"tag"`
+	Branch         string `json:"branch"`
+	CommitRevision string `json:"commitRevision"`
+}
+
+type DependencyRoot struct {
+	Version string         `json:"version"`
+	Deps    DependencyList `json:"deps"`
+}
